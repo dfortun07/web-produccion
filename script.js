c73f7cc7ea19d7f75b994d8c8cd13a578622de6f
dfortun07 <davidfortun05@gmail.com>
2025-12-02 17:22:05 -0400

    Landing page de cine con JS externo

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const boton = document.getElementById("btn-info");
+
+    boton.addEventListener("click", function () {
+        alert("Gracias por visitar Cine Estelar 🎬");
+    });
+});
